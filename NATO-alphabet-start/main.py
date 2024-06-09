e82ad2baed0502b8bc98370d10bb211dbf393f21
mohamed <dhia.hsalem@gmail.com>
2024-06-09 11:56:45 +0200

    game nato phenotic alphabet created

---
+++ NATO-alphabet-start/main.py
@@ -0,0 +1,46 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+letter_file=pandas.read_csv("nato_phonetic_alphabet.csv")
+
+dict={ row.letter:row.code for (index,row) in letter_file.iterrows()
+}
+
+
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_alphabet():
+    name = input("right a name").upper()
+    try:
+        name_listt = [dict[letter] for letter in name]
+
+    except KeyError:
+       print("type in a letter !")
+       generate_alphabet()
+
+    else :
+       print (name_listt)
+
+generate_alphabet()
